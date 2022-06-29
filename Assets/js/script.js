371cff19b2bb52ae9ec242b627905a3b8087edf7
schaparro08 <101684009+schaparro08@users.noreply.github.com>
2022-06-28 21:37:57 -0400

    addition of check boxes

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,10 +3,6 @@ var findTrucks = document.querySelector('#search');
 var foodSelection = document.querySelector('.drops');
 var dropMenu = document.querySelector('.dropdown-menu');
 var dropDown = document.getElementById('dropdown-button');
-// var chinese = document.getElementById('drop1');
-// var mexican = document.getElementById('drop2');
-// var american = document.getElementById('drop3');
-// var indian = document.getElementById('drop4');
 
 
 
@@ -14,26 +10,16 @@ fetch('https://maps.googleapis.com/maps/api/geocode/json?address=Washington&key=
 .then(response => response.json())
 .then(data => console.log(data));
 
-// $(function(){
-  
-//   $(".drops").click(function(){
-    
-//     $("#dropdown-button").text($(this).text());
-//     //  $(".btn:first-child").val($(this).text());
-//   });
-
-// });
 
 
-function changeSelection () {
-  var foodType = foodSelection.textContent;
-  console.log(foodType);
-  // var dropDown = document.getElementById('dropdown-button');
 
-  // dropDown.textContent = foodType;
-  
+function selectOnlyThis(id){
+  var myCheckbox = document.getElementsByName("myCheckbox");
+  Array.prototype.forEach.call(myCheckbox,function(el){
+    el.checked = false;
+  });
+  id.checked = true;
 }
 
 
-foodSelection.addEventListener('click', changeSelection)
 // findTrucks.addEventListener("click",) // add modal function))
