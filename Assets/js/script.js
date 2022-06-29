b8e7f01a3d507a66ff90a3dd724fa17ef0a23b6d
Joshua Diaz <jad24096@gmail.com>
2022-06-28 22:33:01 -0400

    created a second fetch request with input based responses.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -7,26 +7,42 @@ var dropDown = document.getElementById('dropdown-button');
 // var mexican = document.getElementById('drop2');
 // var american = document.getElementById('drop3');
 // var indian = document.getElementById('drop4');
-
+var userZip = document.getElementById('site-search')
+var submit = document.getElementById('submit')
+// var requestOptions = {
+//     method: 'GET',
+//     redirect: 'follow',
+//     mode: 'no-cors'
+//   };
 function search() {
-fetch('https://maps.googleapis.com/maps/api/geocode/json?address=' + zipcode + '&key=AIzaSyDFd1rcFFUqsm2rWJr8kJn_tJXP8SPzmq8')
-.then(response => response.json())
-.then(data => console.log(data));
-
+    var zipcode = userZip.value
+    fetch('https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/geocode/json?address=' + zipcode + '&key=AIzaSyDFd1rcFFUqsm2rWJr8kJn_tJXP8SPzmq8') 
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            console.log(data.results[0].geometry.location.lat)
+            var lat = data.results[0].geometry.location.lat
+            console.log(data.results[0].geometry.location.lng)
+            var lon = data.results[0].geometry.location.lng
+            getTrucks(lat, lon)
+        });       
 }
 
-// });
-
-
-function changeSelection () {
-  var foodType = foodSelection.textContent;
-  console.log(foodType);
-  // var dropDown = document.getElementById('dropdown-button');
-
-  // dropDown.textContent = foodType;
-  
+function getTrucks(lat, lon) {    
+    fetch('https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=coffee+shop&location=' + lat + ',' + lon + '&radius=2000&region=us&type=cafe,bakery&key=AIzaSyCvNkAXwUBzuPBRlTKvVyVuNNujHjGgb88')
+        .then(response => response.json())
+        .then(data => console.log(data));
 }
 
-document.addEventListener('click', search)
-foodSelection.addEventListener('click', changeSelection)
+// function changeSelection() {
+//     var foodType = foodSelection.textContent;
+//     console.log(foodType);
+//     // var dropDown = document.getElementById('dropdown-button');
+
+//     // dropDown.textContent = foodType;
+
+// }
+
+submit.addEventListener('click', search)
+// foodSelection.addEventListener('click', changeSelection)
 // findTrucks.addEventListener("click",) // add modal function))
