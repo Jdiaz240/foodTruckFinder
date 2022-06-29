90c58303725faa1803e532fdecae32f035bd8b56
Joshua Diaz <jad24096@gmail.com>
2022-06-28 21:16:48 -0400

    committing changes before pull.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,19 +1,18 @@
 // query selector for button that says "find food trucks near you"
 var findTrucks = document.querySelector('#search');
+var zipcode;
+
+function search() {
 
 
-
-
-
-fetch('https://maps.googleapis.com/maps/api/geocode/json?address=Washington&key=AIzaSyDFd1rcFFUqsm2rWJr8kJn_tJXP8SPzmq8')
+fetch('https://maps.googleapis.com/maps/api/geocode/json?address=' + zipcode + '&key=AIzaSyDFd1rcFFUqsm2rWJr8kJn_tJXP8SPzmq8')
 .then(response => response.json())
 .then(data => console.log(data));
 
+}
 
 
 
 
 
-
-
-findTrucks.addEventListener("click",) // add modal function))
+findTrucks.addEventListener("click", search) // add modal function))
